df7456e608cd8c89261684dfad1550ce14a922a3
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-08-07 18:16:10 -0500

    added random testing pages

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/navbar'
+
+export default function About() {
+    return (
+        <div>
+            <Navbar />
+            <div>
+                <h1> Howdy! Down yonder, you can call me Sal! </h1>
+            </div>
+        </div>
+    )
+}

612a68002d04f832b324207882b485a0d3fd704e
Salman Malik <salmanmalik@Salmans-Air.attlocal.net>
2020-08-25 04:01:40 -0500

    added color, styled and updated the projects pictures

--- pages/about.js
+++ pages/about.js
@@ -19,55 +19,54 @@ export default function About() {
                 </div>
                 <div className="brand-statement">
                     <p className={styles.description}>
-                        A versatile and growth-oriented learner, I am a software engineer with a creative perspective and a passion for solutions through collaboration.
-                        My previous experience as a tech consultant with an asset management company has allowed for me to hone effective communication skills and leadership to manage client relationships and offer solutions to ensure satisfaction.
+                    A versatile and growth-oriented learner, I am a second-generation software engineer with a passion for creative problem-solving through collaboration and dedication.
                     </p>
                 </div>
                 {/* <div className="personal-description">
                     <p className={styles.description}> I'm a developer, writer, and public speaker with a strong passion for music and singing!</p>
                 </div> */}
-                <div className="peronal-summary">
+                <div className="personal-summary">
                     <p className={styles.description}> I recently graduated from Texas A&M University and worked in business management before making the transition to software engineering.
                         Having recently completed the General Assembly Software Engineering Immersive, I gained hands-on project experience with various tools and technologies.
                     </p>
                 </div>
 
-                <h2>Technologies That I Use:</h2>
+                <h2 className="tech-stacks">Technologies That I Use:</h2>
                 <div className="tech">
                         <div classname="techname">
-                            <img src="html5.png" />
+                            <img src="html5.png" height="75" width="75" />
                             <h4>HTML5</h4>
                         </div>
                         <div classname="techname">
-                            <img src="css.png" />
+                            <img src="css.png" height="75" width="75" />
                             <h4>CSS</h4>
                         </div>
                         <div classname="techname">
-                            <img src="javascript.png" />
+                            <img src="javascript.png" height="75" width="75" />
                             <h4>JavaScript</h4>
                         </div>
                         <div classname="techname">
-                            <img src="react.png" />
+                            <img src="react.png" height="75" width="75" />
                             <h4>React</h4>
                         </div>
                         <div classname="techname">
-                            <img src="sql.png" />
+                            <img src="sql.png" height="75" width="75" />
                             <h4>SQL</h4>
                         </div>
                         <div classname="techname">
-                            <img src="nodejs.png" />
+                            <img src="nodejs.png" height="75" width="75" />
                             <h4>NodeJS</h4>
                         </div>
                         <div classname="techname">
-                            <img src="python.png" />
+                            <img src="python.png" height="75" width="75" />
                             <h4>Python</h4>
                         </div>
                         <div classname="techname">
-                            <img src="typescript.png" />
+                            <img src="typescript.png" height="75" width="75" />
                             <h4>TypeScript</h4>
                         </div>
                         <div classname="techname">
-                            <img src="mongodb.png" />
+                            <img src="mongodb.png" height="75" width="75" />
                             <h4>MongoDB</h4>
                         </div>
                 </div>
